1d4bf69436cd76a829d26cf8c8c71c26e5a35973
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-11-11 12:01:45 -0500

    add view

---
+++ HealthCheck.Mvc/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace HealthCheck.Mvc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHealthCheck(this IServiceCollection services)
+        {
+            // Add views provided in this assembly.   
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Add(
+                    new EmbeddedFileProvider(typeof(HealthCheckController).GetTypeInfo().Assembly));
+            });
+            return services;
+        }
+    }
+}
