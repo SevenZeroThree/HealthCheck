c0b2f1a856ed779b9d927ac2eaaf25bb3fa2d9cc
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-11-11 11:49:50 -0500

    add health check controller

--- HealthCheck.Mvc/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HealthCheck.Mvc
-{
-    public class Class1
-    {
-    }
-}
---
+++ HealthCheck.Mvc/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCheck.Mvc
+{
+    public class HealthCheckController : Controller
+    {
+        [Route("Health")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
