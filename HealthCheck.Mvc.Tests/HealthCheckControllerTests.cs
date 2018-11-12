425aa310b3438f81e875f5ca4546e8e5622c6427
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-11-11 21:39:14 -0500

    add test project

---
+++ HealthCheck.Mvc.Tests/HealthCheckControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace HealthCheck.Mvc.Tests
+{
+    public class HealthCheckControllerTests
+    {
+        private HealthCheckController _controller;
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
