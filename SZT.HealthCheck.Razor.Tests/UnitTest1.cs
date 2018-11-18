6fee2dd73e689b476161a9ef883d4d85d3dd5940
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-11-17 21:27:11 -0500

    add tests

--- SZT.HealthCheck.Razor.Tests/UnitTest1.cs
+++ SZT.HealthCheck.Razor.Tests/UnitTest1.cs
@@ -1,14 +1,43 @@
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace SZT.HealthCheck.Razor.Tests
 {
-    public class UnitTest1
+    public class HealthCheckControllerTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly HealthCheckController _controller;
 
+        public HealthCheckControllerTests()
+        {
+            _controller = new HealthCheckController();
+        }
+
+        [Fact]
+        public void Get_Has_RouteAttribute()
+        {
+            // Arrage
+            var method = _controller.GetType().GetMethod("Get");
+
+            // Act
+            var attribute = (RouteAttribute)method.GetCustomAttributes(typeof(RouteAttribute), false).FirstOrDefault();
+
+            // Assert
+            Assert.Equal("Health", attribute.Template);
+        }
+
+        [Fact]
+        public void Get_Returns_View()
+        {
+            // Arrange
+
+            // Act
+            var result = _controller.Get();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
         }
     }
 }
